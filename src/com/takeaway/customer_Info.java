794476a059a8d72c289cb44ca2dffa4a8ea2e979
ZT837606704 <837606704@qq.com>
2015-11-23 18:41:02 +0800

    修改数据库和相应页面
    商家顾客账户合为用户账号
    用户账号去除坐标

--- src/com/takeaway/customer_Info.java
+++ src/com/takeaway/customer_Info.java
@@ -1,24 +1,14 @@
 package com.takeaway;
 public class customer_Info {
-	public static String dataTable_Name="customer_list";
+	public static String dataTable_Name="user_list";
 	private String userName;
 	private String passWord;
 	private String phone_Number;
-	double coordinate_x ;
-	double coordinate_y ;
 	public customer_Info(String _userName,String _passWord,String _phone_Number)
 	{
 		userName=_userName;
 		passWord=_passWord;
 		phone_Number=_phone_Number;
-	}
-	public customer_Info(String _userName,String _passWord,String _phone_Number,double _coordinate_x,double _coordinate_y)
-	{
-		userName=_userName;
-		passWord=_passWord;
-		phone_Number=_phone_Number;
-		coordinate_x=_coordinate_x;
-		coordinate_y=_coordinate_y;
 	}
 	public static String getDataTable_Name() {
 		return dataTable_Name;
@@ -44,17 +34,5 @@ public class customer_Info {
 	public void setPhone_number(String phone_number) {
 		this.phone_Number = phone_number;
 	}
-	public double getCoordinate_x() {
-		return coordinate_x;
-	}
-	public void setCoordinate_x(double coordinate_x) {
-		this.coordinate_x = coordinate_x;
-	}
-	public double getCoordinate_y() {
-		return coordinate_y;
-	}
-	public void setCoordinate_y(double coordinate_y) {
-		this.coordinate_y = coordinate_y;
-	}
-	
+
 }
