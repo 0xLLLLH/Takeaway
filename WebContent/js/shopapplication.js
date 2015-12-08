64e2e48ec65312f33b5f3336bef8fad03aa977e7
0xLLLLH <linlihao159@gmail.com>
2015-12-08 21:05:19 +0800

    添加我的外卖页面,修正之前的页面

--- WebContent/js/shopapplication.js
+++ WebContent/js/shopapplication.js
@@ -159,3 +159,64 @@ function isSubmitLegal()
 		 if(shop_license=="") document.getElementById("alert_shoplicense").style.display="block";
 	 }
 }
+
+function G(id) {
+	return document.getElementById(id);
+}
+
+	var map = new BMap.Map("l-map");
+	map.centerAndZoom("北京",12);       // 初始化地图,设置城市和地图级别。
+	map.setDefaultCursor("url('bird.cur')");
+	var ac = new BMap.Autocomplete(    //建立一个自动完成的对象
+		{"input" : "suggestId"
+		,"location" : map
+	});
+	
+	ac.addEventListener("onhighlight", function(e) {  //鼠标放在下拉列表上的事件
+	var str = "";
+		var _value = e.fromitem.value;
+		var value = "";
+		if (e.fromitem.index > -1) {
+			value = _value.province +  _value.city +  _value.district +  _value.street +  _value.business;
+		}    
+		str = "FromItem<br />index = " + e.fromitem.index + "<br />value = " + value;
+		
+		value = "";
+		if (e.toitem.index > -1) {
+			_value = e.toitem.value;
+			value = _value.province +  _value.city +  _value.district +  _value.street +  _value.business;
+		}    
+		str += "<br />ToItem<br />index = " + e.toitem.index + "<br />value = " + value;
+		G("searchResultPanel").innerHTML = str;
+	});
+	
+	var myValue;
+	ac.addEventListener("onconfirm", function(e) {    //鼠标点击下拉列表后的事件
+	var _value = e.item.value;
+		myValue = _value.province +  _value.city +  _value.district +  _value.street +  _value.business;
+		G("searchResultPanel").innerHTML ="onconfirm<br />index = " + e.item.index + "<br />myValue = " + myValue;
+		
+		setPlace();
+	});
+function setPlace(){
+	map.clearOverlays();    //清除地图上所有覆盖物
+	function myFun(){
+		var pp = local.getResults().getPoi(0).point;    //获取第一个智能搜索的结果
+		map.centerAndZoom(pp, 18);
+		var marker=new BMap.Marker(pp);
+		map.addOverlay(marker);    //添加标注
+		//marker.addEventListener("click", function(){          
+	}
+	var local = new BMap.LocalSearch(map, { //智能搜索
+	  onSearchComplete: myFun
+	});
+	local.search(myValue);
+}
+
+function hideguider(){
+	$("#tipclose").fadeTo(0,0);
+	$(".guider").fadeTo(0,0);
+}
+function search(){
+		setPlace();
+}
