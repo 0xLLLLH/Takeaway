61a203bd548d5cc796618c66d716410008410b81
0xLLLLH <linlihao159@gmail.com>
2015-12-17 00:02:31 +0800

    店铺页面样式基本完成

--- WebContent/js/itemlist.js
+++ WebContent/js/itemlist.js
@@ -9,3 +9,31 @@ $(".detail").mouseout(function(){
 	$(this).removeClass("detail-shadowed");
 	$(this).addClass("detail");
 });
+
+$(".tab-bar").find("button").click(function (){
+	//alert(this);
+	$(".tab-bar").find("button").removeClass("button-pressed");
+	$(this).addClass("button-pressed");
+	var target= $(this).data("target");
+	//$("#"+target).siblings().hide();
+	$(".tab-container").children().hide();
+	$("."+target).show();
+});
+
+$(".list-item").click(function(){
+	alert($(this).data("id"));
+	var item_name=$(this).find(".title").html();
+	var item_price=$(this).find(".price").html().split('/',2)[0];
+	var new_item='<tr><td>'+item_name
+	+'</td><td><span class="plus fl">-</span><input type="text" class="count fl" value="1"/><span class="plus fl">+</span></td><td>'+
+	item_price+'</td></tr>';
+	$("#cart-items").append(new_item);
+});
+
+$(".clear").click(function(){
+	$("#cart-items").find("tr:gt(0)").empty();
+});
+
+function refresh_money(){
+	
+}
